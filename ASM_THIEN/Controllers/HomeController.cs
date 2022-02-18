84a1ef0a8e102a08e5abf10f2fbc038ed03b2227
Hoàng Tùng <tungnh230802@gmail.com>
2022-02-19 00:42:18 +0700

    first commit

---
+++ ASM_THIEN/Controllers/HomeController.cs
@@ -0,0 +1,51 @@
+﻿using ASM_THIEN.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ASM_THIEN.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Login()
+        {
+            return View();
+        }
+        public IActionResult Cart()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        public IActionResult ProductDetail()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ ASM_THIEN/Models/Product.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ASM_THIEN.Models
+{
+    public class Product
+    {
+        [Key]
+        public int Id { get; set; }
+        [StringLength(255)]
+        public string Name { get; set; }
+
+        [Column(TypeName = "decimal"), Range(1, 100000000)]
+        public float Price { get; set; }
+
+        [Required, Range(1, 9999)]
+        public int Available { get; set; }
+        public string Condition { get; set; }
+        public string Brand { get; set; }
+
+        [Required, StringLength(255)]
+        public string Image { get; set; }
+    }
+}
